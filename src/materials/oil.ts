17b7ba6a2ad89f370125260b39076ceee2346b7d
Oikio <oikiostas@gmail.com>
2020-09-21 23:02:44 +0200

    set prices 1:1

--- src/materials/oil.ts
+++ src/materials/oil.ts
@@ -1,6 +1,6 @@
 import { createResource } from './utils'
 
-createResource('epoxy', { upgrade: 3, price: 5, from: [] })
-createResource('plastic', { upgrade: 3, price: 5, from: [] })
+createResource('epoxy', { upgrade: 3, price: 4, from: [] })
+createResource('plastic', { upgrade: 3, price: 4, from: [] })
 createResource('rubber', { upgrade: 3, price: 5, from: [] })
 createResource('fiberglass', { upgrade: 3, price: 12, from: [{ name: 'plastic', quantity: 4 }, { name: 'glass', quantity: 3 }] })
