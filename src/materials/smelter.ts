5d4b8b5755c73791d452867a76fec622807281f9
Oikio <oikiostas@gmail.com>
2020-09-23 22:37:10 +0200

    pirces for bars

--- src/materials/smelter.ts
+++ src/materials/smelter.ts
@@ -7,14 +7,14 @@ createResource('nails', { upgrade: 2, from: [{ name: 'ironBar', quantity: 1 / 16
 
 // copper
 createResource('copperBar', { upgrade: 2, from: [{ name: 'copperConcentrate', quantity: 0.5 }] })
-createResource('copperPipe', { upgrade: 2, from: [{ name: 'copperBar', quantity: 2 }] })
+createResource('copperPipe', { upgrade: 2, price: 4.5, from: [{ name: 'copperBar', quantity: 2 }] })
 
 // gold
-createResource('goldBar', { upgrade: 2, from: [{ name: 'goldConcentrate', quantity: 0.5 }] })
+createResource('goldBar', { upgrade: 2, price: 7, from: [{ name: 'goldConcentrate', quantity: 0.5 }] })
 
 // steel
 createResource('quicklime', { upgrade: 2, from: [{ name: 'crushedLimestone', quantity: 1 }] })
-createResource('steelBar', { upgrade: 2, from: [{ name: 'coal', quantity: 1 }, { name: 'quicklime', quantity: 1 }, { name: 'ironBar', quantity: 2 }] })
+createResource('steelBar', { upgrade: 2, price: 4.5, from: [{ name: 'coal', quantity: 1 }, { name: 'quicklime', quantity: 1 }, { name: 'ironBar', quantity: 2 }] })
 createResource('rebar', { upgrade: 2, from: [{ name: 'steelBar', quantity: 2 }] })
 createResource('rivet', { upgrade: 2, from: [{ name: 'steelBar', quantity: 1 / 3 }] })
 createResource('steelPipe', { upgrade: 2, from: [{ name: 'steelBar', quantity: 2 }] })
